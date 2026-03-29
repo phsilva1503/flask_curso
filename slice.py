8d55cb77a9d493bbb086f10f526284a312b058ac
phsilva1503 <phsilva1@outlook.com.br>
2026-03-28 22:30:03 -0300

    slice.py

---
+++ slice.py
@@ -0,0 +1,31 @@
+movieName = 'Top Gun'
+# Get the first three characters of the movie name
+firstThreeChars = movieName[:3]
+print(firstThreeChars)  # Output: 'Top'
+# Get the last three characters of the movie name
+lastThreeChars = movieName[-3:]
+print(lastThreeChars)  # Output: 'Gun'
+# Get the characters from index 4 to 6 (inclusive)  
+middleChars = movieName[4:7]
+print(middleChars)  # Output: 'Gun'
+# Get every second character from the movie name
+everySecondChar = movieName[::2]
+print(everySecondChar)  # Output: 'TpG'
+# Get the movie name in reverse order
+reversedMovieName = movieName[::-1]
+print(reversedMovieName)  # Output: 'nuG poT'
+# Get the length of the movie name
+movieNameLength = len(movieName)
+print(movieNameLength)  # Output: 7
+# Get the first character of the movie name
+firstChar = movieName[0]
+print(firstChar)  # Output: 'T' 
+# Get the last character of the movie name
+lastChar = movieName[-1]   
+print(lastChar)  # Output: 'n'
+# Get the characters from index 2 to the end of the movie name
+fromIndexTwoToEnd = movieName[2:]
+print(fromIndexTwoToEnd)  # Output: 'p Gun'
+# Get the characters from the beginning of the movie name to index 4 (exclusive)
+fromStartToIndexFour = movieName[:4]
+
